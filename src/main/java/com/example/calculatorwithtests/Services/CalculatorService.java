0465e19542ebe0b2339f564da77c88ea055d1039
Microd18 <Microd@inbox.ru>
2023-05-10 00:13:01 +0500

    homework done!

--- src/main/java/com/example/calculatorwithtests/Services/CalculatorService.java
+++ src/main/java/com/example/calculatorwithtests/Services/CalculatorService.java
@@ -5,4 +5,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorService {
 
+    public String addition(int a, int b) {
+        return a + " + " + b + " = " + (a + b);
+    }
+
+    public String subtraction(int a, int b) {
+        return a + " - " + b + " = " + (a - b);
+    }
+
+    public String multiply(int a, int b) {
+        return a + " * " + b + " = " + (a * b);
+    }
+
+    public String division(int a, int b) {
+        if(b == 0){
+            throw new IllegalArgumentException("Деление на ноль");
+        }
+        return a + " / " + b + " = " + ((double)a / b);
+    }
 }
--- src/test/java/com/example/calculatorwithtests/CalculatorServiceTest.java
+++ src/test/java/com/example/calculatorwithtests/CalculatorServiceTest.java
@@ -9,5 +9,54 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorServiceTest {
 
+    private CalculatorService calculatorService;
+    private int num1;
+    private int num2;
+    private int num3;
+
+    @BeforeEach
+    public void setUp() {
+        calculatorService = new CalculatorService();
+        num1 = 5;
+        num2 = 6;
+        num3 = 0;
+    }
+
+    @Test
+    public void shouldReturnCorrectAddition(){
+        String expected = num1 + " + " + num2 + " = " + (num1 + num2);
+        String actual = calculatorService.addition(num1, num2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCorrectSubtraction(){
+        String expected = num1 + " - " + num2 + " = " + (num1 - num2);
+        String actual = calculatorService.subtraction(num1, num2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCorrectMultiply(){
+        String expected = num1 + " * " + num2 + " = " + (num1 * num2);
+        String actual = calculatorService.multiply(num1, num2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCorrectDivision(){
+        String expected = num1 + " / " + num2 + " = " + ((double)num1 / num2);
+        String actual = calculatorService.division(num1, num2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenDivisionByZero() {
+        assertThrows(IllegalArgumentException.class,() -> calculatorService.division(num1, num3));
+    }
 
 }
